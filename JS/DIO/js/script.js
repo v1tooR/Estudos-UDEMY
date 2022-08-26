69d787d1561915d46fccd1b2b81add3b6b63146d
v1tooR <victorssantos572@gmail.com>
2022-08-26 17:14:58 -0300

    DIO 2

---
+++ JS/DIO/js/script.js
@@ -0,0 +1,42 @@
+/*const PRECO = 2;
+var desconto = 0.2;
+var total = PRECO - desconto;
+
+console.log(total);*/
+
+/*function soma(a, b){
+    return a + b;
+}
+
+console.log(soma(3, 5))*/
+/*function returnEvenValues(array){
+    
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2!==0){
+            evenNums.push(`${array[i]}`);
+        }else{
+            document.write(`<h3>${array[i]} não é impar!<br><br>`);
+        }
+    }
+    document.write((evenNums));
+}
+let array = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8]
+returnEvenValues(array);*/
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
