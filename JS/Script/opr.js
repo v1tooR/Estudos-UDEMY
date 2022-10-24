d9eca2b9c87cd4245096e43a623ddda47060c3d7
v1tooR <victorssantos572@gmail.com>
2022-10-24 17:12:47 -0300

    card

---
+++ JS/Script/opr.js
@@ -0,0 +1,7 @@
+var num = 9
+var num2 = 10
+
+document.write(num + num2)
+document.write('<br>')
+num += 2
+document.write(num)
---
+++ JS/Script/switch.js
@@ -0,0 +1,14 @@
+var parametro = parseInt(prompt('insira um numero'))
+
+        switch (parametro) {
+            case 1:
+                document.write('resultado')
+                break;
+            // é necessario q seja === (identico)
+            case 2:
+                document.write('resultado2')
+                break;
+            default:
+                document.write('resultado3')
+                break;
+        }
