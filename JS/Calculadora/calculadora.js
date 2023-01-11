7fde8ed814a21364ad6a3727d5cbf8709a79b2b3
v1tooR <victorssantos572@gmail.com>
2023-01-11 19:03:02 -0300

    calculator f

--- JS/Calculadora/calculadora.js
+++ JS/Calculadora/calculadora.js
@@ -6,6 +6,15 @@ function calcular(tipo, valor){
         //limpar visor
         document.getElementById('resultado').value = ''
     }
+    if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+        document.getElementById('resultado').value += valor
+    }
+    if(valor === "="){
+        var valor_campo = eval(document.getElementById('resultado').value)
+        //console.log(eval(valor_campo)) = eval submete qualquer string como codigo script, basicamente transformando a string em numerico
+        document.getElementById('resultado').value = valor_campo
+    
+    }
    }else if(tipo === 'valor'){
     //var valor_campo = document.getElementById('resultado').value
     document.getElementById('resultado').value += valor // concatenar e adicionar
