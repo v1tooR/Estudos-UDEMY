97dc1876347da78fb569daf0a9eb15e0ff900b80
v1tooR <victorssantos572@gmail.com>
2023-03-15 14:26:02 -0300

    js

--- JS/DIO/js/to-do.js
+++ JS/DIO/js/to-do.js
@@ -1,5 +1,7 @@
+// Váriavel que guarda as informações de cada elemteno criando
 var nodeList = document.getElementsByTagName("LI")
 
+// Itens dalista
 for( i = 0; i < nodeList.length; i++){
 	var span = document.createElement("SPAN")
 	var txt = document.createTextNode("\u00D7")
@@ -8,6 +10,7 @@ for( i = 0; i < nodeList.length; i++){
 	nodeList[i].appendChild(span)
 }
 
+// Excluir a tarefa da lista
 var close = document.getElementsByClassName("close")
 for(i = 0; i< close.length; i++){
 	close[i].onclick = function(){
@@ -16,6 +19,7 @@ for(i = 0; i< close.length; i++){
 	}
 }
 
+// Tarefa feita
 var list = document.querySelector('ul')
 list.addEventListener('click', function(ev){
 	if(ev.target.tagName === 'LI'){
@@ -23,15 +27,31 @@ list.addEventListener('click', function(ev){
 	}
 },false	)
 
+// criação dos itens
 function newElement(){
 	var li = document.createElement("li")
 	var inputValue = document.getElementById("myInput").value;
 	var t = document.createTextNode(inputValue)
 	li.appendChild(t)
+	// verifica se há algo escrito ou nao
 	if(inputValue === ''){
 		alert("Escreva algo!")
 	}else{
 		document.getElementById("myUL").appendChild(li)
 	}
+	// pega o valor dentro do campo
 	document.getElementById("myInput").value = "";
+
+	var span = document.createElement("SPAN")
+	var txt = document.createTextNode("\u00D7")
+	span.className = "close"
+	span.appendChild(txt)
+	li.appendChild(span)
+
+	for(i = 0; i< close.length; i++){
+		close[i].onclick = function(){
+			var div = this.parentElement;
+			div.style.display = "none"
+		}
+	}
 }
